601b3d7c8ec3009bac7d02cd6cbb60bcd32f3cff
MalikAhsan-PC <malikahsan13@gmail.com>
2022-12-21 13:57:01 +0500

    added routes and links in app

--- src/App.js
+++ src/App.js
@@ -1,66 +1,96 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
-import Tasks from "./components/Tasks"
+import Tasks from "./components/Tasks";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setTasks(tasksFromServer)
-    }
-    getTasks()
-  },[]);
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
 
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks")
-    const data = await res.json()
-    return data
-  }
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
 
   const onAdd = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks",{
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(task)
-      });
-      const data = await res.json()
-      setTasks([...tasks, data]);
-  }
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
 
   const onUpdReminder = async (id) => {
     const res = await fetch(`http://localhost:5000/tasks/${id}`);
     const taskToToggle = await res.json();
-    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder};
-    await fetch(`http://localhost:5000/tasks/${id}`,{
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    await fetch(`http://localhost:5000/tasks/${id}`, {
       method: "PUT",
       headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(updTask)
+      body: JSON.stringify(updTask),
     });
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !taskToToggle.reminder} : task));
-  }
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !taskToToggle.reminder } : task
+      )
+    );
+  };
 
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`,
-    {
-      method: "DELETE"
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
     });
-    setTasks(tasks.filter((task) => task.id!==id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   return (
-    <div className='container'>
-      <Header onAdd={() => { setShowAddTask(!showAddTask) }} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={onAdd} />}
-      <Tasks tasks={tasks} onUpdReminder={onUpdReminder} deleteTask={deleteTask} />
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => {
+            setShowAddTask(!showAddTask);
+          }}
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Route path="/" element={
+            <>
+              {showAddTask && <AddTask onAdd={onAdd} />}
+              {tasks.length === 0 ? (
+              <Tasks
+                tasks={tasks}
+                onUpdReminder={onUpdReminder}
+                deleteTask={deleteTask}
+          />
+        ) : (
+          "No Tasks to Show"
+        )}
+            </>
+          } />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
